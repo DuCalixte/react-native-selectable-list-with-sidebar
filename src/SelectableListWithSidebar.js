2a4b62cc259da7c4ec89f3938c86932caa662b09
Stanley Calixte <DuCalixte@gmail.com>
2019-01-30 22:36:47 -0500

    second update

--- src/SelectableListWithSidebar.js
+++ src/SelectableListWithSidebar.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, PanResponder } from 'react-native';
+
+export default class SelectableListWithSidebar extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    );
+  }
+}
