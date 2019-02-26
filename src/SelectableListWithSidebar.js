991a7d702c61215f0bd51c693755c8c4e451aa15
Stanley Calixte <DuCalixte@gmail.com>
2019-02-25 19:53:34 -0500

    minor update

--- src/SelectableListWithSidebar.js
+++ src/SelectableListWithSidebar.js
@@ -1,11 +1,29 @@
+'use strict';
+
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, PanResponder } from 'react-native';
+import PropTypes from 'prop-types';
+import {
+  AppRegistry,
+  Image,
+  TouchableHighlight,
+  ScrollView,
+  StyleSheet,
+  Text,
+  PanResponder,
+  TouchableWithoutFeedback,
+  View,
+  Platform,
+} from 'react-native';
+import AlphabetPicker from './AlphabetPicker';
 
 export default class SelectableListWithSidebar extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello world!</Text>
+      <View style={{flex: 1}}>
+        <View>
+          <Text>Hello world!</Text>
+        </View>
+        <AlphabetPicker alphabet={"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')} />
       </View>
     );
   }
